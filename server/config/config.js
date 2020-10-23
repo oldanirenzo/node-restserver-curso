5f037671f2eede0c74886c821c76621fb5a5b1a1
Renzo Oldani <oldanirenzo@gmail.com>
2020-10-23 06:26:20 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -15,10 +15,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // =====================
 let urlDB;
 
-if(process.env.NODE_ENV === 'dev'){
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
-}else{
-    urlDB = 'mongodb+srv://oldanirenzo:T5Ck0xk06ymU6QBv@cafedb.rjhcg.mongodb.net/Cafe'
+} else {
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB;
